b86caa5fb5c9757304b25db5e29d34036293e6cf
Ari5400 <94087682+ArielElbaz@users.noreply.github.com>
2022-09-08 06:46:57 +0300

    111
    1

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import ApprovalToConfirm
-
-
-class ApprovalForm(ModelForm):
-    class Meta:
-        model = ApprovalToConfirm
-        fields = ['image', 'description', 'is_approved', 'user', 'value']
