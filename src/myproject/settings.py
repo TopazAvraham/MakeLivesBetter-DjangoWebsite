ea661d1c7821f015f1c2b4734ee9a18067ac0b54
Ari5400 <94087682+ArielElbaz@users.noreply.github.com>
2023-11-20 06:06:40 +0200

    vxv

--- src/myproject/settings.py
+++ src/myproject/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
@@ -121,18 +121,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_FILE_OVERWRITE = False  
-AWS_DEFAULT_ACL = None  
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'   
-AWS_S3_REGION_NAME = 'eu-west-3'
-AWS_S3_ADDRESSING_STYLE = "virtual"
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_FILE_OVERWRITE = False  
+# AWS_DEFAULT_ACL = None  
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'   
+# AWS_S3_REGION_NAME = 'eu-west-3'
+# AWS_S3_ADDRESSING_STYLE = "virtual"
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.sqlite3',
+# #         'NAME': BASE_DIR / 'db.sqlite3',
+# #     }
+# # }
